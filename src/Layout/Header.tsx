998e0e22c282650e6fa43550779ab938cc8134ce
Thanasis Giannelos <102992298+thanasis-giannelos@users.noreply.github.com>
2023-08-01 19:05:06 +0300

    implement mui

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,26 +1,38 @@
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import React, { useContext } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { CartContext } from "../cart/context/CartContext";
-import classes from "./Header.module.css";
 
 const Header: React.FC = () => {
   const { getTotalAmount } = useContext(CartContext);
+  const numberOfProducts = getTotalAmount();
 
   return (
-    <header className={classes.header}>
-      <nav>
-        <h1>
-          <Link to="/">ReactStore</Link>
-        </h1>
-        <ul>
-          <li>
-            <NavLink to="/cart">
-              Cart {getTotalAmount() > 0 && getTotalAmount()}
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ justifyContent: "space-between"}}>
+          <Typography 
+            variant="h5" 
+            component={Link} 
+            to="/" 
+            sx={{ textDecoration: "none", color: "inherit" }}>
+            ReactStore
+          </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            component={Link}    
+            to="/cart"        
+            sx={{ mr: 2 }}
+          >
+            <ShoppingCartIcon />
+            {numberOfProducts > 0 && <span style={{ fontSize: 13 }}>{numberOfProducts}</span>}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
--- src/products/ProductCategories.tsx
+++ src/products/ProductCategories.tsx
@@ -1,27 +1,43 @@
-import classes from "../Layout/Header.module.css";
+import { Container, Tab, Tabs } from "@mui/material";
 
 interface ProductCategoriesProps {
   categories: string[];
   selectCategory: (category: string) => void;
 }
 
-const ProductCategories: React.FC<ProductCategoriesProps> = ({categories, selectCategory}) => {
+const ProductCategories: React.FC<ProductCategoriesProps> = ({
+  categories,
+  selectCategory,
+}) => {
   return (
-    <ul className={classes.categories}>
-    <li>
-      <a href="#" onClick={() => selectCategory("all_categories")}>
-        all
-      </a>
-    </li>
-    {categories.map((category) => (
-      <li key={category}>
-        <a href="#" onClick={() => selectCategory(`${category}`)}>
-          {category}
-        </a>
-      </li>
-    ))}
-  </ul>
+    <Container sx={{ borderBottom: 1, borderColor: "divider" }}>
+      <Tabs
+        onChange={() => selectCategory("all_categories")}
+        aria-label="basic tabs example"
+      >
+        {categories.map((category) => (
+          <Tab label={category} />
+        ))}
+      </Tabs>
+    </Container>
   );
+
+  // return (
+  //   <ul className={classes.categories}>
+  //   <li>
+  //     <a href="#" onClick={() => selectCategory("all_categories")}>
+  //       all
+  //     </a>
+  //   </li>
+  //   {categories.map((category) => (
+  //     <li key={category}>
+  //       <a href="#" onClick={() => selectCategory(`${category}`)}>
+  //         {category}
+  //       </a>
+  //     </li>
+  //   ))}
+  // </ul>
+  // );
 };
 
 export default ProductCategories;
