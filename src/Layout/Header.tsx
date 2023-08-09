cb7f6d2857d3976cd3347a7def5b2c59bdd209ca
Thanasis Giannelos <102992298+thanasis-giannelos@users.noreply.github.com>
2023-08-09 15:15:16 +0300

    make Header sticky

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,5 +1,5 @@
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../cart/context/CartContext";
@@ -9,30 +9,31 @@ const Header: React.FC = () => {
   const numberOfProducts = getTotalAmount();
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar sx={{ justifyContent: "space-between"}}>
-          <Typography 
-            variant="h5" 
-            component={Link} 
-            to="/" 
-            sx={{ textDecoration: "none", color: "inherit" }}>
-            ReactStore
-          </Typography>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            component={Link}    
-            to="/cart"        
-            sx={{ mr: 2 }}
-          >
-            <ShoppingCartIcon />
-            {numberOfProducts > 0 && <span style={{ fontSize: 13 }}>{numberOfProducts}</span>}
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="sticky" sx={{top: 0, zIndex: 100}}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Typography
+          variant="h5"
+          component={Link}
+          to="/"
+          sx={{ textDecoration: "none", color: "inherit" }}
+        >
+          ReactStore
+        </Typography>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          component={Link}
+          to="/cart"
+          sx={{ mr: 2 }}
+        >
+          <ShoppingCartIcon />
+          {numberOfProducts > 0 && (
+            <span style={{ fontSize: 13 }}>{numberOfProducts}</span>
+          )}
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 };
 
