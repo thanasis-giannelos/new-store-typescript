72379a9255b3f70e2846c51fcd79e649a9b69898
Thanasis Giannelos <athgiannelos@gmail.com>
2023-08-14 12:34:23 +0300

    update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
