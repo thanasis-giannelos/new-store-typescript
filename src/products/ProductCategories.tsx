ba0455df46f2cadbe6169611842abbbc466cb447
Thanasis Giannelos <athgiannelos@gmail.com>
2023-09-04 14:14:33 +0300

    get categories from database

--- src/products/ProductCategories.tsx
+++ src/products/ProductCategories.tsx
@@ -1,14 +1,33 @@
 import { Button, Container, Stack } from "@mui/material";
+import { useEffect, useState } from "react";
+import { ref, child, get } from "firebase/database";
+import { database } from "../firebase-config";
+
 
 interface ProductCategoriesProps {
-  categories: string[];
   selectCategory: (category: string) => void;
 }
 
 const ProductCategories: React.FC<ProductCategoriesProps> = ({
-  categories,
   selectCategory,
 }) => {
+  const [categories, setCategories] = useState<string[]>([]);
+
+  useEffect(() => {
+    const dbRef = ref(database);
+    get(child(dbRef, `product_categories`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        setCategories(snapshot.val());
+      } else {
+        console.log("No product categories available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }, []);
+
   return (
     <Container maxWidth="sm" sx={{ marginY: "20px" }}>
       <Stack direction="row" sx={{ justifyContent: "space-between" }}>
