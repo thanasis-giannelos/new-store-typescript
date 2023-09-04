30166329d0b6c2b96af3a4a1e43a83a9c0e1e981
Thanasis Giannelos <athgiannelos@gmail.com>
2023-09-04 13:31:08 +0300

    implement Product Type

--- src/products/product.ts
+++ src/products/product.ts
@@ -1,30 +1,15 @@
-class Product {
-  id: string;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: {[key: string]: number};
-
-  constructor(
-    id: string,
-    title: string,
-    price: number,
-    description: string,
-    category: string,
-    image: string,
-    rating: {rate: number, count: number}
-  ) {
-    this.id = id;
-    this.title = title;
-    this.price =  price;
-    this.description =  description;
-    this.category =  category;
-    this.image =  image;
-    this.rating = {rate: rating.rate, count: rating.count}
+type Product = {
+  [id: string]: {
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: {
+      rate: number,
+      count: number
+    };  
   }
-
 }
 
 export default Product;
