8ece0c43f63943966b6107b05930d2fc25423ea4
Thanasis Giannelos <102992298+thanasis-giannelos@users.noreply.github.com>
2023-09-11 17:54:17 +0300

    implement categories menu

--- src/Layout/header/Header.tsx
+++ src/Layout/header/Header.tsx
@@ -4,31 +4,116 @@ import {
   AppBar,
   Badge,
   Box,
+  Button,
   IconButton,
   Toolbar,
-  Typography,
+  Typography
 } from "@mui/material";
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../auth/AuthContext";
 import { CartContext } from "../../cart/CartContext";
+import './Menu.css';
 
 const Header: React.FC = () => {
   const { getTotalAmount } = useContext(CartContext);
   const { signedInUser } = useContext(AuthContext);
   const numberOfProducts = getTotalAmount();
 
+  const [showMenu, setShowMenu] = React.useState(false);
+
+  const handleMenu = () => {
+    setShowMenu(!showMenu)
+  };
+
+  // const handleClose = () => {
+  //   setAnchorEl(null);
+  // };
+  const categories = [
+    "smartphones",
+    "laptops",
+    "fragrances",
+    "skincare",
+    "groceries",
+    "home-decoration",
+    "furniture",
+    "tops",
+    "womens-dresses",
+    "womens-shoes",
+    "mens-shirts",
+    "mens-shoes",
+    "mens-watches",
+    "womens-watches",
+    "womens-bags",
+    "womens-jewellery",
+    "sunglasses",
+    "automotive",
+    "motorcycle",
+    "lighting",
+  ];
+
   return (
-    <AppBar position="sticky" sx={{ backgroundColor: 'white', color: 'black', boxShadow: 'none', top: 0, zIndex: 100 }}>
-      <Toolbar sx={{ width: '80%', justifyContent: "space-between", margin: '0 auto' }}>
-        <Typography
-          variant="h5"
-          component={Link}
-          to="/"
-          sx={{ textDecoration: "none", color: "inherit" }}
-        >
-          ReactStore
-        </Typography>
+    <AppBar
+      position="sticky"
+      sx={{
+        backgroundColor: "white",
+        color: "black",
+        boxShadow: "none",
+        top: 0,
+        zIndex: 100,
+      }}
+    >
+      <Toolbar
+        sx={{ width: "80%", justifyContent: "space-between", margin: "0 auto" }}
+      >
+        <Box width="20rem" display="flex" justifyContent="space-between">
+          <Typography
+            variant="h4"
+            component={Link}
+            to="/"
+            sx={{
+              textDecoration: "none",
+              color: "inherit",
+              fontWeight: "bold",
+            }}
+          >
+            ReactStore
+          </Typography>
+          <div className='menu-container'>
+            <Button  className='menu-btn' onClick={handleMenu}>Categories</Button>
+            {/* <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              {categories.map((cat) => (
+                <MenuItem onClick={handleClose}>{cat}</MenuItem>
+              ))}
+            </Menu>
+ */}
+            <ul className={`menu-list ${!showMenu && 'not-active'}`}>
+              {categories.map((cat) => {
+                return (
+                  <li className='menu-item'>
+                    <Link to="#" style={{ textDecoration: "none" }}>
+                      {cat}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </Box>
         <Box>
           {signedInUser && (
             <Typography
