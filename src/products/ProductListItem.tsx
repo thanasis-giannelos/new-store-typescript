d4f88d14805427df1e85a8331eb06d41bb356337
Thanasis Giannelos <athgiannelos@gmail.com>
2023-09-14 20:16:37 +0300

    wip

--- src/products/ProductListItem.tsx
+++ src/products/ProductListItem.tsx
@@ -1,5 +1,5 @@
-import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
-import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import {
   Card,
   CardActionArea,
@@ -7,7 +7,7 @@ import {
   CardContent,
   CardMedia,
   IconButton,
-  Typography
+  Typography,
 } from "@mui/material";
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
@@ -19,9 +19,7 @@ type ProductListItemProps = {
 };
 
 const ProductListItem: React.FC<ProductListItemProps> = ({ item }) => {
-  // const [productId] = Object.keys(item);
-  // const [productProps] = Object.values(item);
-  const {id, title, thumbnail, price, rating} = item;
+  const { id, title, thumbnail, price, rating } = item;
 
   const { addToCart, removeFromCart } = useContext(CartContext);
 
@@ -29,12 +27,8 @@ const ProductListItem: React.FC<ProductListItemProps> = ({ item }) => {
     addToCart(item);
   }
 
-  // function removeFromCartBtnHandler() {
-  //   removeFromCart(item);
-  // }
-
   return (
-    <li style={{ listStyle: "none", width: 200, flex: "1 1 0" }}>
+    <li style={{ listStyle: "none" }}>
       <Card>
         <CardActionArea component={Link} to={"/" + id}>
           <CardMedia
@@ -43,28 +37,30 @@ const ProductListItem: React.FC<ProductListItemProps> = ({ item }) => {
             image={thumbnail}
             sx={{ objectFit: "contain" }}
           />
-          <CardContent>
-            <Typography gutterBottom variant="h6" component="div" noWrap>
+          <CardContent sx={{ fontSize: "1rem" }}>
+            <h5
+              style={{
+                overflow: "hidden",
+                whiteSpace: "nowrap",
+                textOverflow: "ellipsis",
+              }}
+            >
               {title}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              $ {price}
-            </Typography>
-            <Typography variant="subtitle2" color="text.secondary">
-              <span>
-                <StarBorderOutlinedIcon sx={{ height: 0.8 }} /> {rating}
-              </span>
-            </Typography>
+            </h5>
+            <h3>$ {price}</h3>
+            <h5 style={{ display: "inline-block" }}>
+              <StarBorderOutlinedIcon fontSize="inherit" />
+              {rating}
+            </h5>
           </CardContent>
         </CardActionArea>
         <CardActions sx={{ justifyContent: "space-around" }}>
           <IconButton
-            size="medium"
-            edge="start"
+            size="large"
             color="inherit"
             onClick={addToCartBtnHandler}
           >
-            <AddShoppingCartOutlinedIcon/>
+            <AddShoppingCartOutlinedIcon />
           </IconButton>
         </CardActions>
       </Card>
